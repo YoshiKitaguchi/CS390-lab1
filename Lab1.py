1707482485b6c13adee8f9be07aa6168b15a2029
YoshiKitaguchi <ykita0224@gmail.com>
2020-03-07 23:19:24 -0500

    commit2

--- Lab1.py
+++ Lab1.py
@@ -2,10 +2,13 @@
 import os
 import numpy as np
 import tensorflow as tf
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt; plt.rcdefaults()
+
 from tensorflow import keras
 from tensorflow.keras.utils import to_categorical
 import random
-
 
 random.seed(1618)
 np.random.seed(1618)
@@ -15,14 +18,14 @@ tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 #ALGORITHM = "guesser"
-#ALGORITHM = "tf_net"
+# ALGORITHM = "tf_net"
 ALGORITHM = "tf_conv"
 
 DATASET = "mnist_d"
-#DATASET = "mnist_f"
-#DATASET = "cifar_10"
-#DATASET = "cifar_100_f"
-#DATASET = "cifar_100_c"
+# DATASET = "mnist_f"
+# DATASET = "cifar_10"
+# DATASET = "cifar_100_f"
+# DATASET = "cifar_100_c"
 
 if DATASET == "mnist_d":
     NUM_CLASSES = 10
@@ -49,9 +52,12 @@ elif DATASET == "cifar_100_f":
     IW = 32
     IZ = 3
     IS = 3072
-    # TODO: Add this case.
 elif DATASET == "cifar_100_c":
-    pass                                 # TODO: Add this case.
+    NUM_CLASSES = 100
+    IH = 32
+    IW = 32
+    IZ = 3
+    IS = 3072
 
 
 #=========================<Classifier Functions>================================
@@ -65,9 +71,9 @@ def guesserClassifier(xTest):
     return np.array(ans)
 
 
-def buildTFNeuralNet(x, y, eps = 6):
+def buildTFNeuralNet(x, y, eps = 20):
     #TODO: Implement a standard ANN here.
-    model = keras.Sequential([keras.layers.Flatten(), keras.layers.Dense(512, activation=tf.nn.relu), tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+    model = keras.Sequential([keras.layers.Flatten(), keras.layers.Dense(512, activation=tf.nn.relu), keras.layers.Dense(1024, activation=tf.nn.relu), keras.layers.Dense(256, activation=tf.nn.relu), tf.keras.layers.Dense(NUM_CLASSES, activation=tf.nn.softmax)])
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     invert_categorical_y = np.argmax(y, axis=1)
     model.fit(x, invert_categorical_y, epochs=eps, verbose=0)
@@ -77,16 +83,30 @@ def buildTFNeuralNet(x, y, eps = 6):
 def buildTFConvNet(x, y, eps = 10, dropout = True, dropRate = 0.2):
     #TODO: Implement a CNN here. dropout option is required.
     model = keras.Sequential()
+
     inShape = (IH, IW, IZ)
     opt = tf.optimizers.Adam()
-    model.add(keras.layers.Conv2D(32, kernel_size=(3, 3), activation = "relu", input_shape=inShape))
-    model.add(keras.layers.Conv2D(32, kernel_size=(3, 3), activation="relu"))
+    model.add(keras.layers.Conv2D(32, kernel_size=(3, 3), activation = "elu", input_shape=inShape))
+    model.add(keras.layers.Conv2D(32, kernel_size=(3, 3), activation="elu"))
     model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(keras.layers.Dropout(dropRate, noise_shape=None, seed=None))
+
+    # model.add(keras.layers.Conv2D(64, kernel_size=(3, 3), activation="elu", input_shape=inShape))
+    # model.add(keras.layers.Conv2D(64, kernel_size=(3, 3), activation="elu"))
+    # model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    # model.add(keras.layers.Dropout(dropRate, noise_shape=None, seed=None))
+    #
+    # model.add(keras.layers.Conv2D(128, kernel_size=(3, 3), activation="elu", input_shape=inShape))
+    # model.add(keras.layers.Conv2D(128, kernel_size=(3, 3), activation="elu"))
+    # model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    # model.add(keras.layers.Dropout(dropRate, noise_shape=None, seed=None))
+
     model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(128, activation="relu"))
     if dropout:
         model.add(keras.layers.Dropout(dropRate, noise_shape=None, seed=None))
-    model.add(keras.layers.Dense(128, activation="relu"))
     model.add(keras.layers.Dense(NUM_CLASSES, activation=tf.nn.softmax))
+
     model.compile(optimizer = opt, loss = 'sparse_categorical_crossentropy', metrics=['accuracy'])
     invert_categorical_y = np.argmax(y, axis=1)
     model.fit(x, invert_categorical_y, eps)
@@ -192,6 +212,27 @@ def evalResults(data, preds):
     print()
 
 
+def plot_ANN ():
+    objects = ['mnist_d', 'mnist_f', 'cifar_10', 'cifar_100_f', 'cifar_100_c']
+    y_pos = np.arange(len(objects))
+    performance = [97.56, 87.44, 10, 1, 1]
+
+    plt.bar(y_pos, performance, align='center', alpha=0.5)
+    plt.xticks(y_pos, objects)
+    plt.ylabel('accuracy (percentage)')
+    plt.title('ANN')
+    plt.show()
+
+def plot_CNN ():
+    objects = ['mnist_d', 'mnist_f', 'cifar_10', 'cifar_100_f', 'cifar_100_c']
+    y_pos = np.arange(len(objects))
+    performance = [98.31, 85.22, 10, 1, 21.46]
+
+    plt.bar(y_pos, performance, align='center', alpha=0.5)
+    plt.xticks(y_pos, objects)
+    plt.ylabel('accuracy (percentage)')
+    plt.title('CNN')
+    plt.show()
 
 #=========================<Main>================================================
 
@@ -201,6 +242,10 @@ def main():
     model = trainModel(data[0])
     preds = runModel(data[1][0], model)
     evalResults(data[1], preds)
+
+    plot_ANN();
+    plot_CNN();
+
 
 
 
